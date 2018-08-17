e9ec175024a16b796b155d5545ae34347b10e468
Severin Beauvais <severin.beauvais@gmail.com>
2018-08-17 14:32:59 -0700

    PRC-574: returned applications instead of sidsFound

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -1,32 +1,32 @@
-var auth        = require("../helpers/auth");
-var _           = require('lodash');
-var defaultLog  = require('winston').loggers.get('default');
-var mongoose    = require('mongoose');
-var Actions     = require('../helpers/actions');
-var request     = require('request');
+var auth       = require("../helpers/auth");
+var _          = require('lodash');
+var defaultLog = require('winston').loggers.get('default');
+var mongoose   = require('mongoose');
+var Actions    = require('../helpers/actions');
+var request    = require('request');
 
 exports.protectedOptions = function (args, res, rest) {
   res.status(200).send();
-}
+};
 
 exports.publicGetClientsInfoByDispositionId = function (args, res, next) {
   var dtId = args.swagger.params.dtId.value;
   defaultLog.info("Searching arcgis for client info on Disposition Transaction ID:", dtId);
-  
+
   var searchURL = "http://maps.gov.bc.ca/arcgis/rest/services/mpcm/bcgw/MapServer/dynamicLayer/query?layer=%7B%22id%22%3A1%2C%22source%22%3A%7B%22type%22%3A%22dataLayer%22%2C%22dataSource%22%3A%7B%22type%22%3A%22table%22%2C%22workspaceId%22%3A%22MPCM_ALL_PUB%22%2C%22dataSourceName%22%3A%22WHSE_TANTALIS.TA_INTEREST_HOLDER_VW%22%7D%7D%7D&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&returnDistinctValues=false&f=json&where=DISPOSITION_TRANSACTION_SID=";
   return new Promise(function (resolve, reject) {
-    request({url: searchURL + "'" + dtId + "'"}, function (err, res, body) {
+    request({ url: searchURL + "'" + dtId + "'" }, function (err, res, body) {
       if (err) {
         reject(err);
       } else if (res.statusCode !== 200) {
-        reject(res.statusCode+' '+body);
+        reject(res.statusCode + ' ' + body);
       } else {
         var obj = {};
         try {
-          defaultLog.info ('ArcGIS Call Complete.', body);
+          defaultLog.info('ArcGIS Call Complete.', body);
           obj = JSON.parse(body);
         } catch (e) {
-          defaultLog.error ('Parsing Failed.', e);
+          defaultLog.error('Parsing Failed.', e);
         }
         var clients = [];
         _.each(obj.features, function (i) {
@@ -38,7 +38,7 @@ exports.publicGetClientsInfoByDispositionId = function (args, res, next) {
   }).then(function (data) {
     return Actions.sendResponse(res, 200, data);
   }).catch(function (err) {
-    defaultLog.error (err);
+    defaultLog.error(err);
     return Actions.sendResponse(res, 400, err);
   });
 };
@@ -52,50 +52,53 @@ exports.publicGetBCGW = function (args, res, next) {
 
   // var searchURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_TANTALIS.TA_CROWN_TENURES_SVW/ows?service=wfs&version=2.0.0&request=getfeature&typename=pub:WHSE_TANTALIS.TA_CROWN_TENURES_SVW&outputFormat=application/json&PROPERTYNAME=CROWN_LANDS_FILE&CQL_FILTER=CROWN_LANDS_FILE=";
   var searchURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_TANTALIS.TA_CROWN_TENURES_SVW/ows?service=wfs&version=2.0.0&request=getfeature&typename=PUB:WHSE_TANTALIS.TA_CROWN_TENURES_SVW&outputFormat=json&srsName=EPSG:4326&CQL_FILTER=CROWN_LANDS_FILE=";
-  console.log("SEARCHING:", searchURL+ "'" + clFile + "'");
+  console.log("SEARCHING:", searchURL + "'" + clFile + "'");
   return new Promise(function (resolve, reject) {
-    request({url: searchURL + "'" + clFile + "'"}, function (err, res, body) {
+    request({ url: searchURL + "'" + clFile + "'" }, function (err, res, body) {
       if (err) {
         reject(err);
       } else if (res.statusCode !== 200) {
-        reject(res.statusCode+' '+body);
+        reject(res.statusCode + ' ' + body);
       } else {
         var obj = {};
         try {
-          defaultLog.info ('BCGW Call Complete.', body);
+          defaultLog.info('BCGW Call Complete.', body);
           obj = JSON.parse(body);
         } catch (e) {
-          defaultLog.error ('Parsing Failed.', e);
+          defaultLog.error('Parsing Failed.', e);
           resolve(obj);
         }
 
         // Search for this in our DB in case it's been imported in an application.
         try {
           var result = _.chain(obj.features)
-          .groupBy("properties.DISPOSITION_TRANSACTION_SID")
-          .toPairs()
-          .map(function(currentItem) {
+            .groupBy("properties.DISPOSITION_TRANSACTION_SID")
+            .toPairs()
+            .map(function (currentItem) {
               return _.zipObject(["SID", "sids"], currentItem);
-          })
-          .value();
+            })
+            .value();
 
-          obj.sidsFound = [];
-          result.reduce (function (current, code) {
-            return current.then (function () {
-                // console.log ('+++++++++', code.SID);
-                var Application = require('mongoose').model('Application');
-                return Application.findOne({tantalisID: code.SID}, function (err, o) {
+          obj.applications = [];
+          result.reduce(function (current, code) {
+            return current.then(function () {
+              // TODO: change following to new function/promise
+              //       and second 'then' to another function/promise
+              var Application = require('mongoose').model('Application');
+              return Application.findOne({ tantalisID: code.SID }, function (err, o) {
                 if (o) {
-                  obj.sidsFound.push(code.SID);
+                  obj.applications.push(o);
                 } else {
                   console.log("Nothing found");
                 }
-              });
+              })
+                // .then() // TODO: another search by CLID here
+                ;
             });
           }, Promise.resolve())
-          .then(function () {
-            resolve(obj);
-          });
+            .then(function () {
+              resolve(obj);
+            });
         } catch (e) {
           // Error, don't tag the isImported on it.
           resolve(obj);
@@ -105,7 +108,7 @@ exports.publicGetBCGW = function (args, res, next) {
   }).then(function (data) {
     return Actions.sendResponse(res, 200, data);
   }).catch(function (err) {
-    defaultLog.error (err);
+    defaultLog.error(err);
     return Actions.sendResponse(res, 400, err);
   });
 };
@@ -119,18 +122,18 @@ exports.publicGetBCGWDispositionTransactionId = function (args, res, next) {
 
   var searchURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_TANTALIS.TA_CROWN_TENURES_SVW/ows?service=wfs&version=2.0.0&request=getfeature&typename=PUB:WHSE_TANTALIS.TA_CROWN_TENURES_SVW&outputFormat=json&srsName=EPSG:4326&CQL_FILTER=DISPOSITION_TRANSACTION_SID=";
   return new Promise(function (resolve, reject) {
-    request({url: searchURL + "'" + dtId + "'"}, function (err, res, body) {
+    request({ url: searchURL + "'" + dtId + "'" }, function (err, res, body) {
       if (err) {
         reject(err);
       } else if (res.statusCode !== 200) {
-        reject(res.statusCode+' '+body);
+        reject(res.statusCode + ' ' + body);
       } else {
         var obj = {};
         try {
-          defaultLog.info ('BCGW Call Complete.', body);
+          defaultLog.info('BCGW Call Complete.', body);
           obj = JSON.parse(body);
         } catch (e) {
-          defaultLog.error ('Parsing Failed.', e);
+          defaultLog.error('Parsing Failed.', e);
         }
         resolve(obj);
       }
@@ -138,7 +141,7 @@ exports.publicGetBCGWDispositionTransactionId = function (args, res, next) {
   }).then(function (data) {
     return Actions.sendResponse(res, 200, data);
   }).catch(function (err) {
-    defaultLog.error (err);
+    defaultLog.error(err);
     return Actions.sendResponse(res, 400, err);
   });
 };
