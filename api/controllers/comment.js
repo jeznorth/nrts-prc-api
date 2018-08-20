a0b028a5d246eb23c954975beb612f5bf21df462
Severin Beauvais <severin.beauvais@gmail.com>
2018-08-20 16:16:11 -0700

    PRC-597: added support for comment paging

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -5,52 +5,91 @@ var mongoose    = require('mongoose');
 var Actions     = require('../helpers/actions');
 var Utils       = require('../helpers/utils');
 
+var DEFAULT_PAGESIZE = 100;
+var MAX_LIMIT = 1000;
+
 exports.protectedOptions = function (args, res, rest) {
   res.status(200).send();
-}
+};
 
 exports.publicGet = function (args, res, next) {
-  // Build match query if on CommentId route
   var query = {};
-  if (args.swagger.params.CommentId && args.swagger.params.CommentId.value) {
-    query = Utils.buildQuery("_id", args.swagger.params.CommentId.value, query);
-  }
-  if (args.swagger.params._commentPeriod && args.swagger.params._commentPeriod.value) {
-    query = Utils.buildQuery("_commentPeriod", args.swagger.params._commentPeriod.value, query);
-  }
+  var skip = null, limit = null;
+
+  // Never return deleted comment(s).
   _.assignIn(query, { isDeleted: false });
-  defaultLog.info("query:", query);
 
-  getComments(['public'], query, args.swagger.params.fields.value)
-  .then(function (data) {
-    return Actions.sendResponse(res, 200, data);
-  });
-};
-exports.protectedGet = function(args, res, next) {
-  var Comment = mongoose.model('Comment');
-
-  defaultLog.info("args.swagger.params:", args.swagger.params.auth_payload.scopes);
-
-  // Build match query if on CommentId route
-  var query = {};
+  // Build match query if on CommentId route.
   if (args.swagger.params.CommentId && args.swagger.params.CommentId.value) {
     query = Utils.buildQuery("_id", args.swagger.params.CommentId.value, query);
   }
-  if (args.swagger.params._commentPeriod && args.swagger.params._commentPeriod.value) {
-    query = Utils.buildQuery("_commentPeriod", args.swagger.params._commentPeriod.value, query);
+  else {
+    if (args.swagger.params._commentPeriod && args.swagger.params._commentPeriod.value) {
+      query = Utils.buildQuery("_commentPeriod", args.swagger.params._commentPeriod.value, query);
+    }
+
+    var pageSize = DEFAULT_PAGESIZE;
+    if (args.swagger.params.pageSize && args.swagger.params.pageSize.value !== undefined) {
+      if (args.swagger.params.pageSize.value > 0) {
+        pageSize = args.swagger.params.pageSize.value;
+      }
+    }
+    if (args.swagger.params.pageNum && args.swagger.params.pageNum.value !== undefined) {
+      if (args.swagger.params.pageNum.value >= 0) {
+        skip = (args.swagger.params.pageNum.value * pageSize);
+        limit = pageSize;
+      }
+    }
   }
-  // Unless they specifically ask for it, hide deleted results.
-  if (args.swagger.params.isDeleted && args.swagger.params.isDeleted.value != undefined) {
-    _.assignIn(query, { isDeleted: args.swagger.params.isDeleted.value });
+
+  // defaultLog.info("query:", query);
+  getComments(['public'], query, args.swagger.params.fields.value, skip, limit)
+    .then(function (data) {
+      return Actions.sendResponse(res, 200, data);
+    });
+};
+
+exports.protectedGet = function (args, res, next) {
+  // defaultLog.info("args.swagger.params:", args.swagger.params.auth_payload.scopes);
+
+  var query = {};
+  var skip = null, limit = null;
+
+  // Unless they specifically ask for it, don't return deleted comment(s).
+  if (args.swagger.params.isDeleted && args.swagger.params.isDeleted.value === true) {
+    _.assignIn(query, { isDeleted: true });
   } else {
     _.assignIn(query, { isDeleted: false });
   }
-  defaultLog.info("query:", query);
 
-  getComments(args.swagger.params.auth_payload.scopes, query, args.swagger.params.fields.value)
-  .then(function (data) {
-    return Actions.sendResponse(res, 200, data);
-  });
+  // Build match query if on CommentId route.
+  if (args.swagger.params.CommentId && args.swagger.params.CommentId.value) {
+    query = Utils.buildQuery("_id", args.swagger.params.CommentId.value, query);
+  }
+  else {
+    if (args.swagger.params._commentPeriod && args.swagger.params._commentPeriod.value) {
+      query = Utils.buildQuery("_commentPeriod", args.swagger.params._commentPeriod.value, query);
+    }
+
+    var pageSize = DEFAULT_PAGESIZE;
+    if (args.swagger.params.pageSize && args.swagger.params.pageSize.value !== undefined) {
+      if (args.swagger.params.pageSize.value > 0) {
+        pageSize = args.swagger.params.pageSize.value;
+      }
+    }
+    if (args.swagger.params.pageNum && args.swagger.params.pageNum.value !== undefined) {
+      if (args.swagger.params.pageNum.value >= 0) {
+        skip = (args.swagger.params.pageNum.value * pageSize);
+        limit = pageSize;
+      }
+    }
+  }
+
+  // defaultLog.info("query:", query);
+  getComments(args.swagger.params.auth_payload.scopes, query, args.swagger.params.fields.value, skip, limit)
+    .then(function (data) {
+      return Actions.sendResponse(res, 200, data);
+    });
 };
 
 //  Create a new Comment
@@ -83,10 +122,10 @@ exports.unProtectedPost = function (args, res, next) {
   // comment._addedBy = args.swagger.params.auth_payload.userID;
 
   comment.save()
-  .then(function (c) {
-    // defaultLog.info("Saved new Comment object:", c);
-    return Actions.sendResponse(res, 200, c);
-  });
+    .then(function (c) {
+      // defaultLog.info("Saved new Comment object:", c);
+      return Actions.sendResponse(res, 200, c);
+    });
 };
 
 // Update an existing Comment
@@ -95,6 +134,7 @@ exports.protectedPut = function (args, res, next) {
   defaultLog.info("ObjectID:", args.swagger.params.CommentId.value);
 
   var obj = args.swagger.params.comment.value;
+
   // Strip security tags - these will not be updated on this route.
   delete obj.tags;
   if (obj.review) {
@@ -107,13 +147,14 @@ exports.protectedPut = function (args, res, next) {
       obj.review.tags = [['sysadmin']]
     }
   }
+
   if (obj.commentAuthor) {
     delete obj.commentAuthor.tags;
     // Did they request anon?
     if (obj.commentAuthor.requestedAnonymous) {
       obj.commentAuthor.tags = [['sysadmin']];
     } else {
-      obj.commentAuthor.tags = [['sysadmin'],['public']];
+      obj.commentAuthor.tags = [['sysadmin'], ['public']];
     }
 
     // Never allow this to be updated
@@ -123,13 +164,11 @@ exports.protectedPut = function (args, res, next) {
     }
   }
 
-  console.log('object:', obj);
-
   defaultLog.info("Incoming updated object:", obj);
   // TODO sanitize/update audits.
 
   var Comment = require('mongoose').model('Comment');
-  Comment.findOneAndUpdate({_id: objId}, obj, {upsert:false, new: true}, function (err, o) {
+  Comment.findOneAndUpdate({ _id: objId }, obj, { upsert: false, new: true }, function (err, o) {
     if (o) {
       defaultLog.info("o:", o);
       return Actions.sendResponse(res, 200, o);
@@ -138,7 +177,7 @@ exports.protectedPut = function (args, res, next) {
       return Actions.sendResponse(res, 404, {});
     }
   });
-}
+};
 
 // Publish/Unpublish the Comment
 exports.protectedPublish = function (args, res, next) {
@@ -146,108 +185,113 @@ exports.protectedPublish = function (args, res, next) {
   defaultLog.info("Publish Comment:", objId);
 
   var Comment = require('mongoose').model('Comment');
-  Comment.findOne({_id: objId}, function (err, o) {
+  Comment.findOne({ _id: objId }, function (err, o) {
     if (o) {
       defaultLog.info("o:", o);
 
       // Add public to the tag of this obj.
       Actions.publish(o)
-      .then(function (published) {
-        // Published successfully
-        return Actions.sendResponse(res, 200, published);
-      }, function (err) {
-        // Error
-        return Actions.sendResponse(res, err.code, err);
-      });
+        .then(function (published) {
+          // Published successfully
+          return Actions.sendResponse(res, 200, published);
+        }, function (err) {
+          // Error
+          return Actions.sendResponse(res, err.code, err);
+        });
     } else {
       defaultLog.info("Couldn't find that object!");
       return Actions.sendResponse(res, 404, {});
     }
   });
 };
+
 exports.protectedUnPublish = function (args, res, next) {
   var objId = args.swagger.params.CommentId.value;
   defaultLog.info("UnPublish Comment:", objId);
 
   var Comment = require('mongoose').model('Comment');
-  Comment.findOne({_id: objId}, function (err, o) {
+  Comment.findOne({ _id: objId }, function (err, o) {
     if (o) {
       defaultLog.info("o:", o);
 
       // Remove public to the tag of this obj.
       Actions.unPublish(o)
-      .then(function (unpublished) {
-        // UnPublished successfully
-        return Actions.sendResponse(res, 200, unpublished);
-      }, function (err) {
-        // Error
-        return Actions.sendResponse(res, err.code, err);
-      });
+        .then(function (unpublished) {
+          // UnPublished successfully
+          return Actions.sendResponse(res, 200, unpublished);
+        }, function (err) {
+          // Error
+          return Actions.sendResponse(res, err.code, err);
+        });
     } else {
       defaultLog.info("Couldn't find that object!");
       return Actions.sendResponse(res, 404, {});
     }
   });
 };
-var getComments = function (role, query, fields) {
+
+var getComments = function (role, query, fields, skip, limit) {
   return new Promise(function (resolve, reject) {
     var Comment = mongoose.model('Comment');
     var projection = {};
 
     // Fields we always return
-    var defaultFields = ['_id',
-                        'tags'];
+    var defaultFields = [
+      '_id',
+      'tags'
+    ];
     _.each(defaultFields, function (f) {
-        projection[f] = 1;
+      projection[f] = 1;
     });
 
     // Add requested fields - sanitize first by including only those that we can/want to return
     var sanitizedFields = _.remove(fields, function (f) {
-      return (_.indexOf(['name',
-                        'commentNumber',
-                        'comment',
-                        'internal',
-                        'dateAdded',
-                        'commentAuthor',
-                        'review',
-                        '_addedBy',
-                        '_commentPeriod',
-                        'review',
-                        'commentStatus',
-                        'isDeleted'], f) !== -1);
+      return (_.indexOf([
+        'name',
+        'commentNumber',
+        'comment',
+        'internal',
+        'dateAdded',
+        'commentAuthor',
+        'review',
+        '_addedBy',
+        '_commentPeriod',
+        'review',
+        'commentStatus',
+        'isDeleted'
+      ], f) !== -1);
     });
     _.each(sanitizedFields, function (f) {
       projection[f] = 1;
     });
 
     Comment.aggregate([
-      {
-        "$match": query
-      },
-      {
-        "$project": projection
-      },
+      { "$match": query },
+      { "$project": projection },
       {
         $redact: {
-         $cond: {
+          $cond: {
             if: {
               $anyElementTrue: {
-                    $map: {
-                      input: "$tags" ,
-                      as: "fieldTag",
-                      in: { $setIsSubset: [ "$$fieldTag", role ] }
-                    }
-                  }
-                },
-              then: "$$DESCEND",
-              else: "$$PRUNE"
-            }
+                $map: {
+                  input: "$tags",
+                  as: "fieldTag",
+                  in: { $setIsSubset: ["$$fieldTag", role] }
+                }
+              }
+            },
+            then: "$$DESCEND",
+            else: "$$PRUNE"
           }
         }
+      },
+      { "$sort": { _id: 1 } },
+      { "$skip": skip || 0 },
+      { "$limit": limit || MAX_LIMIT }
     ]).exec()
-    .then(function (data) {
-      defaultLog.info("data:", data);
-      resolve(data);
-    });
+      .then(function (data) {
+        defaultLog.info("data:", data);
+        resolve(data);
+      });
   });
 };
