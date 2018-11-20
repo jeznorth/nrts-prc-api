8bf331c7543e51207efddd28869dcc5025c86154
Andrew Whillans <Andrew.Whillans@gov.bc.ca>
2018-11-20 09:54:05 -0800

    Correctly pass in mongoose_options when starting up mongo test db server

--- config/mongoose_options.js
+++ config/mongoose_options.js
@@ -8,3 +8,4 @@ const mongooseOptions = {
     connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
     socketTimeoutMS: 45000 // Close sockets after 45 seconds of inactivity
 }
+exports.mongooseOptions = mongooseOptions;
